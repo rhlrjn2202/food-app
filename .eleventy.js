dd1042f7c9e0b5b03525cff57c873769808353de
rhlrjn2202 <46061702+rhlrjn2202@users.noreply.github.com>
2024-11-21 22:12:45 +0530

    5

--- .eleventy.js
+++ .eleventy.js
@@ -1,33 +1,10 @@
-const { v4: uuidv4 } = require('uuid');
-    const { format, parseISO } = require('date-fns');
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src");
 
-    module.exports = function(eleventyConfig) {
-      eleventyConfig.addPassthroughCopy("src");
-
-      eleventyConfig.addCollection("menu", function(collection) {
-        return collection.getFilteredByGlob("src/menu/*.md");
-      });
-
-      eleventyConfig.addCollection("orders", function(collection) {
-        return collection.getFilteredByGlob("src/orders/*.md");
-      });
-
-      eleventyConfig.addCollection("users", function(collection) {
-        return collection.getFilteredByGlob("src/users/*.md");
-      });
-
-      eleventyConfig.addShortcode("uuid", function() {
-        return uuidv4();
-      });
-
-      eleventyConfig.addFilter("formatDate", function(date) {
-        return format(parseISO(date), 'MMM d, yyyy');
-      });
-
-      return {
-        dir: {
-          input: "src",
-          output: "dist"
-        }
-      };
-    };
+  return {
+    dir: {
+      input: "src",
+      output: "dist"
+    }
+  };
+};
